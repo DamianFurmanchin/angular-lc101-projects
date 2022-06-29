425f220951ff032787f4ecc37bcf16a18f083c75
DamianFurmanchin <103968889+DamianFurmanchin@users.noreply.github.com>
2022-06-29 17:16:26 -0400

    half way point

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'My Photo Choices';
+  image1 = 'https://m.media-amazon.com/images/M/MV5BY2ViNDIwNmUtZGQ5Ni00MDYxLWI5YTktNWU1NGRlYWQ1ZDg0XkEyXkFqcGdeQXVyOTAyMDgxODQ@._V1_SX300_.jpg';
+  image2 = 'https://www.logolynx.com/images/logolynx/s_23/231ef856b95f23dddffcf61effcb99e3.jpeg';
+  image3 = 'https://cdn.worldvectorlogo.com/logos/aphex-twin.svg';
 
   constructor() { }
 
