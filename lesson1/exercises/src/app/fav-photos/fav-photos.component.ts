481d7e3e74edef5086c3926d105b50e0ca26f958
DamianFurmanchin <103968889+DamianFurmanchin@users.noreply.github.com>
2022-06-29 17:43:44 -0400

    final

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'My Photo Choices';
+  photosTitle = 'Three Random Photos';
   image1 = 'https://m.media-amazon.com/images/M/MV5BY2ViNDIwNmUtZGQ5Ni00MDYxLWI5YTktNWU1NGRlYWQ1ZDg0XkEyXkFqcGdeQXVyOTAyMDgxODQ@._V1_SX300_.jpg';
   image2 = 'https://www.logolynx.com/images/logolynx/s_23/231ef856b95f23dddffcf61effcb99e3.jpeg';
   image3 = 'https://cdn.worldvectorlogo.com/logos/aphex-twin.svg';
